c9aa68e600657cc21960a7e191cdef5ce88dc7cc
auto <batsura.vs@gmail.com>
2023-06-14 00:05:06 +0300

    test build

--- app/android/build-scripts/build.sh
+++ app/android/build-scripts/build.sh
@@ -3,7 +3,7 @@ cd proxy_master || exit 1
 #flutter build apk --release
 #mv ./build/app/outputs/flutter-apk/app-release.apk ./release.apk
 cd ..
-./version "$()"
+./version "$MESSAGE"
 
 git config --global user.email "batsura.vs@gmail.com"
 git config --global user.name "auto"
