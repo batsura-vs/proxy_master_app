e7c9b57e9bb29f110eba981b95326cb97bf42b4a
auto <batsura.vs@gmail.com>
2023-06-14 00:40:20 +0300

    test build

--- app/android/build-scripts/build.sh
+++ app/android/build-scripts/build.sh
@@ -2,16 +2,16 @@ git clone "https://$GITHUB_TOKEN@github.com/batsura-vs/proxy_master"
 cd proxy_master || exit 1
 #flutter build apk --release
 #mv ./build/app/outputs/flutter-apk/app-release.apk ./release.apk
-cd ../app/android || exit 1
-rm -rf ../../proxy_master
+rm -rf proxy_master
 git status
 git rebase --continue
-./build-scripts/version "$MESSAGE"
+./app/android/build-scripts/version "$MESSAGE"
+mv version.json ./app/android/version.json
 cat version.json
 git config --global user.email "batsura.vs@gmail.com"
 git config --global user.name "auto"
-#git add ./release.apk
-git add ./version.json
-#git commit ./release.apk
-git commit ./version.json
+#git add ./app/android/release.apk
+git add ./app/android/version.json
+#git commit ./app/android/release.apk
+git commit ./app/android/version.json
 git push
