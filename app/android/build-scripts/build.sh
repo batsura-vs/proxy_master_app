b919138cce94bfef39c2458578fff40babbb8849
auto <batsura.vs@gmail.com>
2023-06-14 00:44:51 +0300

    test build

--- app/android/build-scripts/build.sh
+++ app/android/build-scripts/build.sh
@@ -4,15 +4,11 @@ cd proxy_master || exit 1
 #mv ./build/app/outputs/flutter-apk/app-release.apk ./release.apk
 cd ..
 rm -rf proxy_master
-git status
-git rebase --continue
 ./app/android/build-scripts/version "$MESSAGE"
-mv version.json ./app/android/version.json
-cat version.json
 git config --global user.email "batsura.vs@gmail.com"
 git config --global user.name "auto"
 #git add ./app/android/release.apk
 git add ./app/android/version.json
-#git commit ./app/android/release.apk
-git commit ./app/android/version.json
+#git commit -m "$(date +'%H:%M (%d-%m-%y)')"  ./app/android/release.apk
+git commit -m "$(date +'%H:%M (%d-%m-%y)')"  ./app/android/version.json
 git push
