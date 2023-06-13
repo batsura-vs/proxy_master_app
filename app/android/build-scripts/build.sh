3fa77d9970d9767ee0a7964d72b9a2295cbf6244
auto <batsura.vs@gmail.com>
2023-06-14 00:23:15 +0300

    test build

--- app/android/build-scripts/build.sh
+++ app/android/build-scripts/build.sh
@@ -4,8 +4,8 @@ cd proxy_master || exit 1
 #mv ./build/app/outputs/flutter-apk/app-release.apk ./release.apk
 cd ../app/android || exit
 ./build-scripts/version "$MESSAGE"
-
-git remote set-url origin https://x-access-token:"$GITHUB_TOKEN"@github.com/batsura-vs/proxy_master_app
+ls
+cat version.json
 git config --global user.email "batsura.vs@gmail.com"
 git config --global user.name "auto"
 #git add ./release.apk
