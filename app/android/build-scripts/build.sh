3f5b901c9b5fdf4b8ae7e90062e750ddd7af4f10
auto <batsura.vs@gmail.com>
2023-06-14 00:28:33 +0300

    test build

--- app/android/build-scripts/build.sh
+++ app/android/build-scripts/build.sh
@@ -2,9 +2,9 @@ git clone "https://$GITHUB_TOKEN@github.com/batsura-vs/proxy_master"
 cd proxy_master || exit 1
 #flutter build apk --release
 #mv ./build/app/outputs/flutter-apk/app-release.apk ./release.apk
-cd ../app/android || exit
+cd ../app/android || exit 1
+rm ../../proxy_master
 ./build-scripts/version "$MESSAGE"
-ls
 cat version.json
 git config --global user.email "batsura.vs@gmail.com"
 git config --global user.name "auto"
