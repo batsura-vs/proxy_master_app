ed8a922fdd32c01d35aacbb95d99507097c47305
auto <batsura.vs@gmail.com>
2023-06-14 13:19:13 +0300

    test build

--- app/android/build-scripts/build.sh
+++ app/android/build-scripts/build.sh
@@ -4,6 +4,7 @@ flutter build apk --release
 mv ./build/app/outputs/flutter-apk/app-release.apk ../app/android/release.apk
 cd ..
 ./app/android/build-scripts/version "$MESSAGE" ./proxy_master/pubspec.yaml
+mv ./proxy_master/CHANGELOG.md ./app/android/description.md
 rm -rf proxy_master
 mv ./version.json ./app/android/version.json
 git config --global user.email "batsura.vs@gmail.com"
