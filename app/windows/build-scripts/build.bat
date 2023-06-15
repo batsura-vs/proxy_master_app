3db6d33089a1a759603d0a3c9fae46d0a149f0fb
voven4ek <batsura.vs@gmail.com>
2023-06-15 23:03:36 +0300

    update

--- app/windows/build-scripts/build.bat
+++ app/windows/build-scripts/build.bat
@@ -2,12 +2,12 @@ git clone https://%GITHUB_TOKEN%@github.com/batsura-vs/proxy_master
 cd proxy_master
 call flutter create --platforms=windows,macos,linux .
 call flutter build windows
-.\Appacker_v1.3.11.exe -s "build\windows\runner\Release" -e ".\proxy_master.exe"
+call .\Appacker_v1.3.11.exe -s "build\windows\runner\Release" -e ".\proxy_master.exe"
 mv build\windows\runner\proxy_master.exe ..\app\windows\proxy_master.exe
 cd ..
-rm proxy_master
+rmdir proxy_master
 git config --global user.email "batsura.vs@gmail.com"
 git config --global user.name "auto"
 git add -A
-git commit -m %date%
+git commit -m "%date%"
 git push
